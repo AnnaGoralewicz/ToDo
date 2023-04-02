96fa6d57d6334029af1399f95176dc6c749e01be
AnnaGoralewicz <anna.goralewicz@icloud.com>
2023-04-02 21:40:30 +0200

    first version with db connection

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+var db = require("mariadb");
+
+var pool = db.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "secret",
+  database: "TODO_DB",
+});
+
+BigInt.prototype["toJSON"] = function () {
+  return parseInt(this.toString());
+};
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   logger.info("req get");
   const todo = {
@@ -18,6 +34,30 @@ app.get("/", (req, res) => {
   };
 
   res.send(todo);
+});
+
+app.post("/add", (req, res) => {
+  logger.info("add new todo");
+  logger.debug(req.body);
+  pool.getConnection().then((conn) => {
+    conn
+      .query("INSERT INTO TODO (description, done) VALUES(?, ?)", [
+        req.body.description,
+        false,
+      ])
+      .then((dbRes) => {
+        logger.info("new todo entry ");
+        res.json({
+          id: dbRes.insertId,
+          description: req.body.description,
+          done: false,
+        });
+      })
+      .catch((err) => {
+        logger.error(err.message);
+        res.status(500).json({ error: err.message });
+      });
+  });
 });
 
 app.listen(port, () => {
